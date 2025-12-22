d6e86fa54c9691df4d19f12efa81ebe49a24748c
BONNAURE Olivier <o.bonnaure@solisoft.net>
2025-12-22 09:42:19 +0100

    feat: new coumound indexes + doc

---
+++ www/public/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'talks-v1';
+const urlsToCache = [
+    '/',
+    '/logo.svg',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
