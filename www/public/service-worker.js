ffe1b911f2e1eb2174214843394e1d4cbbd0eade
BONNAURE Olivier <o.bonnaure@solisoft.net>
2025-12-22 20:29:02 +0100

    feat: talks: update service worker

--- www/public/service-worker.js
+++ www/public/service-worker.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = 'talks-v2';
-const urlsToCache = [
-    '/',
-    '/logo.svg',
-    '/manifest.json'
-];
+const CACHE_NAME = "talks-v3";
+const urlsToCache = ["/", "/logo.svg", "/manifest.json"];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
